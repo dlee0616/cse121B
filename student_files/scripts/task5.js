9f67c8afb00f9d7aae4ed1a71804f241c3622226
dlee0616 <98242430+dlee0616@users.noreply.github.com>
2022-10-19 15:30:47 -0700

    progress for task5

--- student_files/scripts/task5.js
+++ student_files/scripts/task5.js
@@ -5,19 +5,17 @@
 // Step 1: Declare and initialize a new variable to hold the current date
 let currentDate = new Date();
 // Step 2: Declare another variable to hold the day of the week
-let day;
-// Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
-currenDate.getDay("monday");
+let day = currenDate.getDay();
+// Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() 
 // Step 4: Declare a variable to hold a message that will be displayed
-let msg = "";
+let msg="";
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
-if  (currentDate = "monday"){
-    msg = ("Hang in there!")
+if  (currentDate === 0 || currentDate === 6){
+    msg = ("woohoo! It's the weekend")
 }
 else {
-    msg =("Woohoo! its the weekend")
+    msg = ("hang in there!")
 }
-
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
 
 /* SWITCH, CASE, BREAK */
@@ -26,19 +24,19 @@ else {
 let message = "";
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
 switch (day){
-    case "monday":
+    case 1:
         message = "monday";
-    case "tuesday":
+    case 2:
         message = "tuesday";
-    case "wednesday":
+    case 3:
         message = "wednesday";
-    case "thursday":
+    case 4:
         message = "thursday";
-    case "friday":
+    case 5:
         message = "friday";
-    case "saturday":
+    case 6:
         message = "saturday";
-    case "sunday":
+    case 7:
         message = "sunday";
     break;
 }
@@ -51,7 +49,8 @@ document.getElementById('message1').innerHTML = msg;
 document.getElementById('message2').innerHTML = message;
 /* FETCH */
 // Step 1: Declare a global empty array variable to store a list of temples
-const temples = ["idaho falls", "slc temple", "NYC temple"];
+let text = "";
+const templeData = [];
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
 // - Creates an HTML <article> element
 // - Creates an HTML <h3> element and add the temple's templeName property to it
@@ -61,21 +60,61 @@ const temples = ["idaho falls", "slc temple", "NYC temple"];
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
 function output(temples){
-    for (let i = 0; i < temples.length; i++){ 
-        document.getElementById('tmepleName').innerHTML =
-         "<article>"+
-         "<h3>">templeName<"</h3>"+
-         "<h4>">location<"</h4>"+
-        "<img src = imageURL alt = templeName</img>"
-        "</article>"
+    reset();
+    temples.forEach(temples => {
+        let article = document.createElement('article');
+        let templeName = document.createElement('h3');
+        let dedicated = document.createElement('h4');
+        let location = document.createElement('h4');
+
+        templeName.textcontent = temples.templeName;
+        location.textContent = temples.location; 
+        dedicated.textContent = temples.dedicated;
+        
+        let photo = document.createElement('picture');
+        let image = document.createElement('image');
+        
+        article.appendChild(templeName);
+        article.appendChild(location);
+        article.appendChild(photo);
+        article.appendChild(dedicated);
+        
+        image.setAttribute('src', temples.imageURL);
+        image.setAtttribute('alt', temples.templeName);
+        photo.appendChild(image);
+
+
+
+    })
+
 }
 // Step 3: Create another function called getTemples. Make it an async function.
+const url = 'https://byui-cse.github.io/cse121b-course/week05/temples.json';
+async function getTemples(url){
+    const answer = await fetch(url)
+    if (answer.ok ){
+        templeData = await answer.json();
+        output(templeData);
+    }
+    else {
+        console.log("problem with API")
+    }
+}
+
 // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
-// Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
+// Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.  
 // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
 
 // Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
-
+const reset = (temples) => {
+    temples.forEach (temples => {
+        document.getElementById('#temples').innerHTML = "";
+    })
+}
+function sortBy(){
+   
+}
+element.addEventListener('change', sortBy())
 // Step 8: Declare a function named sortBy that does the following:
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
